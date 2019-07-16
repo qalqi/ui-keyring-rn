cc435d2d217b43e2c78debf8530a63e172f1a01f
qalqi <qalqi@qalqi.com>
2019-07-17 01:32:49 +0530

    Added missing dependency

---
+++ index.js
@@ -0,0 +1,16 @@
+// Copyright 2017-2019 @polkadot/ui-keyring authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+
+import { assertSingletonPackage } from '@polkadot/util';
+
+import keyring, { Keyring } from './src/Keyring';
+export * from './src/stores';
+
+assertSingletonPackage('@polkadot/ui-keyring');
+
+export default keyring;
+
+export {
+  Keyring
+};
